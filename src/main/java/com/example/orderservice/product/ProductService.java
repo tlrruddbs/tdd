93fc501fce5439dcdb70b86406aacd5953224f54
tlrruddbs <tlrruddbs@naver.com>
2024-04-02 01:51:14 +0900

    상품등록 테스트 진행중

--- src/main/java/com/example/orderservice/product/ProductService.java
+++ src/main/java/com/example/orderservice/product/ProductService.java
@@ -1,9 +1,14 @@
 package com.example.orderservice.product;
 
-import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Component
+
+@RestController
 class ProductService {
 
     private final ProductPort productPort;
@@ -13,10 +18,13 @@ class ProductService {
     }
 
 
-    @Transactional
-    public void addProduct(AddProductRequest request) {
+//    @Transactional
+    @PostMapping("/products")
+    public ResponseEntity<Void> addProduct(@RequestBody final AddProductRequest request) {
         final Product product = new Product(request.name(), request.price(), request.discountPolicy());
         productPort.save(product);
+
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
 
