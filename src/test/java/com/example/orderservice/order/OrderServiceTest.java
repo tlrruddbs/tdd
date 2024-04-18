8e5c4ca3c95a92bf4ed6f87f71c5105e6aa8a2b8
tlrruddbs <tlrruddbs@naver.com>
2024-04-18 23:42:59 +0900

    API테스트로 전환하기

--- src/test/java/com/example/orderservice/order/OrderServiceTest.java
+++ src/test/java/com/example/orderservice/order/OrderServiceTest.java
@@ -1,17 +1,24 @@
 package com.example.orderservice.order;
 
+import com.example.orderservice.ApiTest;
 import com.example.orderservice.product.DiscountPolicy;
 import com.example.orderservice.product.Product;
 import com.example.orderservice.product.ProductService;
 import com.example.orderservice.product.ProductSteps;
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 
-@SpringBootTest
-class OrderServiceTest {
+import static org.assertj.core.api.Assertions.assertThat;
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class OrderApiTest extends ApiTest {
     @Autowired
     private OrderService orderService;
     @Autowired
@@ -19,10 +26,19 @@ class OrderServiceTest {
 
     @Test
     void 상품주문() {
-        productService.addProduct(ProductSteps.상품등록요청_생성());
+        ProductSteps.상품등록요청(ProductSteps.상품등록요청_생성());
         final CreateOrderRequest request = 상품주문요청_생성();
 
-        orderService.createOrder(request); 
+        ExtractableResponse<Response> response = RestAssured.given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(request)
+                .when()
+                .post("/orders")
+                .then()
+                .log().all().extract();
+
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+
     }
 
     private static CreateOrderRequest 상품주문요청_생성() {
